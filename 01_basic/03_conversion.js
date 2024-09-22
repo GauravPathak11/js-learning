e9b09b532ad0981cb64a84697b0f32c4c8f99bf3
Gaurav Pathak <121251982+GauravPathak11@users.noreply.github.com>
2024-09-22 20:18:13 +0000

    conversion of data type

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,23 @@
+let score = "33"
+//console.log(typeof score);
+
+let valueinnumber = Number(score) // like a to_string in string when we convert one datatype to another
+//console.log(typeof valueinnumber)//used to define the datatype in js
+
+/*
+"33" = 33
+"33abc" = NaN (not a number)
+true = 1 false =0
+*/
+
+// let isLoggedIn =1
+let isLoggedIn ="" // empty h to false else true
+
+
+let booleanisloggedIn = Boolean(isLoggedIn)
+//console.log(booleanisloggedIn)
+
+let sum = 33
+let stringNumber = String(sum)
+
+console.log(typeof stringNumber)
