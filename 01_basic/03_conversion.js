b9c7792fc18a14f658c9813b437099d7f09712aa
Gaurav Pathak <121251982+GauravPathak11@users.noreply.github.com>
2024-09-22 20:35:14 +0000

    conversion

--- 01_basic/03_conversion.js
+++ 01_basic/03_conversion.js
@@ -20,4 +20,29 @@ let booleanisloggedIn = Boolean(isLoggedIn)
 let sum = 33
 let stringNumber = String(sum)
 
-console.log(typeof stringNumber)
+//console.log(typeof stringNumber)
+
+
+/* **********************opertaion ************************ */
+
+let str1 = "ram "
+let str2 = "ram"
+
+let str3 =str1+str2
+//console.log(str3);
+
+// console.log("1"+2)//output is 12
+// console.log(1+"2") // output is 12
+// console.log("1"+2+2) // ouput is 122 reason is strt with string than js think whole is a string 
+// console.log(1+2+"2") // output is 32 reason strt with number than js think whole is number 
+
+// console.log("1+2+2"+5)
+
+// console.log(+true)//used for conversion
+
+let num1,num2,num3
+num1 = num2 =num3 = 2+1
+
+let x=100
+x++
+console.log(x)
