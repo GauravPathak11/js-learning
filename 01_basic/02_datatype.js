2d8cbed57d93f7e1c38b940d4839e9d85e7fe56d
Gaurav Pathak <121251982+GauravPathak11@users.noreply.github.com>
2024-09-22 20:02:52 +0000

    datatype and variable

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,19 @@
+"use strict" //treat all js code as newer version
+
+//alert("hello") we are using  node js not browser so it not run directly in node js if we want this then it have differnt syntax in node js but in browser we can do it directly
+
+let name = "Gaurav"
+let age= 15
+let isLoggedIn = false;
+
+// number =>2 to power 53
+//bigint
+//boolean = True or False
+// strin = " "
+//null = standlone or we can say that empty
+//undefined = not defined
+// symbol =uniqueness
+
+//object
+
+console.log(typeof null)
---
+++ 01_basic/var_const.js
@@ -0,0 +1,7 @@
+const accountID = 12345
+let accountEmail = "Gaurav@google.com"
+var accountPassword = "1234567"
+accountCity = "Hathras"
+
+// accountID =5
+console.table([accountID,accountEmail,accountPassword,accountCity]);
